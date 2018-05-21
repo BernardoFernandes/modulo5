71bfa729c5096f21b8f6c49d326f83d57a25e958
BernardoFernandes <32074494+BernardoFernandes@users.noreply.github.com>
2018-05-21 11:56:42 +0100

    Add files via upload

---
+++ Livroasssinaturas.java
@@ -0,0 +1,48 @@
+
+package livroasssinaturas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Livroasssinaturas {
+private static List <String> assinaturas;
+	
+	public static void main(String[] args) {
+		assinaturas = new ArrayList <String>();
+		Scanner dados = new Scanner (System.in);
+		int menu;
+		String inserir;
+		do {
+		System.out.print("..:Assinaturas:.."
+				+"\n(1) Adicionar Assinaturas \n"
+				+"(2) Visualizar Assisnaturas \n"
+				+"(0) Sair do programa \n"
+				+"\nInsira a opção que deseja: ");
+		menu = dados.nextInt();
+		switch(menu) {
+			case 1: 
+					System.out.println();
+					inserir=JOptionPane.showInputDialog("Assinatura: ");
+					assinaturas.add(inserir);
+					System.out.println();
+					System.out.println(assinaturas);
+					System.out.println();
+			break;
+			case 2:
+					System.out.println();
+					System.out.println(assinaturas);
+					System.out.println();
+			break;
+			case 0:
+					System.exit(0);
+					
+		}
+		} while (true);
+
+	}
+	}    
+    
+    
+
